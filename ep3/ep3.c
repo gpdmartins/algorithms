4477fdea23857bd5dc6ae40711ed51dbbd4485b0
Jose Guilherme Martins <gpdmartins@gmail.com>
2017-01-25 20:11:42 -0200

    ep3 finalizado

--- ep3/ep3.c
+++ ep3/ep3.c
@@ -1,3 +1,8 @@
+/*Nome: Jose Guilherme Pereira Dias Martins
+**Curso: Topicos de Programação
+**Curso de Verão 2017
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,7 +28,10 @@ void empilha (char conteudo, No **pilha){
     
     No *novo = mallocSafe(sizeof(No));
     novo->conteudo = conteudo;
-    novo->prox = *pilha;
+    if((*pilha)== NULL)
+        novo->prox = NULL;
+    else
+        novo->prox = *pilha;
     *pilha = novo;
 }
 char desempilha (No **pilha){
@@ -31,45 +39,61 @@ char desempilha (No **pilha){
     char retorno = pop->conteudo;
     *pilha = (*pilha)->prox;
     free(pop);
+    /*printf("desempilhando: %c\n", retorno);*/
     return retorno;
 }
 void imprimePilha (No *pilha){
-    if(pilha != NULL)
-        printf("%c ", pilha->conteudo);
-        imprimePilha (pilha->prox);
-}
+    while(pilha->prox != NULL){
+        printf("%c", pilha->conteudo);
+            pilha = pilha->prox;
+    }
+        printf("%c", pilha->conteudo);
+    }
+
 int bemFormada (char str[]){
-    No *colchete, *chave, *parentese;
+    No *pilha = NULL;
     int i, tamanho;
     tamanho = strlen(str);
 
     for (i=0;i<tamanho;i++){
         switch (str[i]){
-            case '(':
-                empilha (str[i], &parentese);
-            break;
-            case ')':
-                if(desempilha (&parentese) != '(') return 0;
-            break;
-            case '[':
-                empilha (str[i], &colchete);
-            break;
-            case ']':
-                if (desempilha (&colchete) != ']') return 0;
-            break;
-            case '{':
-                empilha(str[i], &chave);
-            break;
-            case '}':
-                if (desempilha (&chave) != '{') return 0;
+            
+            case '(':{
+            empilha(str[i], &pilha); break;}
+            case ')':{
+            if(pilha == NULL || desempilha(&pilha)!= '(') return 0; break;}
+            case '[':{
+            empilha(str[i], &pilha); break;}
+            case ']':{
+            if(pilha == NULL || desempilha(&pilha)!= '[') return 0; break;}
+            case '{':{
+            empilha(str[i], &pilha); break;}
+            case '}':{
+            if(pilha == NULL || desempilha(&pilha)!= '{') return 0; break;}
         }
     }
+    if(pilha != NULL)
+        return 0;
+    
     return 1;
 
 
 }
 
-
+int main(void){
+    char str[] = "{[()]}";
+    No *pilha = NULL;
+    int i =0;
+    
+    for(i=0;i<strlen(str);i++){
+        empilha(str[i], &pilha);
+    }    
+    
+    imprimePilha(pilha);
+    printf("\n");
+    printf("\n%d\n", bemFormada(str));
+    return 0;
+}
 
 
 
